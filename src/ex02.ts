2378f85275b086300b83d9999cc3d89f898eeafb
Hebert Soares <hebertsoaresof@gmail.com>
2023-10-13 19:42:58 -0300

    Criando todas funções de paradigma

--- src/ex02.ts
+++ src/ex02.ts
@@ -13,67 +13,129 @@ let pessoas: Pessoa[] = [
   { id: 4, nome: "Nicolau Copérnico", biografia: "Nicolau Copérnico foi um astrônomo e matemático polonês que desenvolveu a teoria heliocêntrica do Sistema Solar." }
 ];
 
-// Função para obter a biografia de uma pessoa pelo ID
-function obterBiografiaPorID(id: number, listaPessoas: Pessoa[]): string {
+// Função para obter a biografia de uma pessoa pelo ID no paradigma funcional
+const obterBiografiaPorIDFuncional = (id: number, listaPessoas: Pessoa[]): string => {
   const pessoa = listaPessoas.find(pessoa => pessoa.id === id);
   return pessoa ? pessoa.biografia : "Pessoa não encontrada.";
+};
+
+// Função para obter a biografia de uma pessoa pelo ID no paradigma imperativo
+function obterBiografiaPorIDImperativo(id: number, listaPessoas: Pessoa[]): string {
+  for (let i = 0; i < listaPessoas.length; i++) {
+    if (listaPessoas[i].id === id) {
+      return listaPessoas[i].biografia;
+    }
+  }
+  return "Pessoa não encontrada.";
 }
 
-// Função para obter o nome de uma pessoa pelo ID
-function obterNomePorID(id: number): string {
-  const pessoa = pessoas.find(p => p.id === id);
+// Função para obter o nome de uma pessoa pelo ID no paradigma funcional
+const obterNomePorIDFuncional = (id: number, listaPessoas: Pessoa[]): string => {
+  const pessoa = listaPessoas.find(pessoa => pessoa.id === id);
   return pessoa ? pessoa.nome : "Pessoa não encontrada.";
+};
+
+// Função para obter o nome de uma pessoa pelo ID no paradigma imperativo
+function obterNomePorIDImperativo(id: number, listaPessoas: Pessoa[]): string {
+  for (let i = 0; i < listaPessoas.length; i++) {
+    if (listaPessoas[i].id === id) {
+      return listaPessoas[i].nome;
+    }
+  }
+  return "Pessoa não encontrada.";
 }
 
-// Função para remover uma pessoa da lista pelo ID
-function removerPessoaPorID(id: number): string {
+// Função para remover uma pessoa da lista pelo ID no paradigma funcional
+const removerPessoaPorIDFuncional = (id: number, listaPessoas: Pessoa[]): string => {
+  const pessoaEncontrada = listaPessoas.find(pessoa => pessoa.id === id);
+
+  if (pessoaEncontrada) {
+    const nomePessoaRemovida = pessoaEncontrada.nome;
+    const novaLista = listaPessoas.filter(pessoa => pessoa.id !== id);
+    listaPessoas.length = 0;
+    novaLista.forEach(pessoa => listaPessoas.push(pessoa));
+    exibirRegistros(listaPessoas); // Atualiza a lista no HTML
+    return `${nomePessoaRemovida} foi removido(a).`;
+  } else {
+    return "Pessoa não encontrada.";
+  }
+};
+
+// Função para remover uma pessoa da lista pelo ID no paradigma imperativo
+function removerPessoaPorIDImperativo(id: number, listaPessoas: Pessoa[]): string {
   let indiceParaRemover = -1;
 
-  // Encontra o índice do objeto especificado pelo ID
-  for (let i = 0; i < pessoas.length; i++) {
-    if (pessoas[i].id === id) {
+  for (let i = 0; i < listaPessoas.length; i++) {
+    if (listaPessoas[i].id === id) {
       indiceParaRemover = i;
       break;
     }
   }
 
-  // Remove o objeto no índice encontrado
   if (indiceParaRemover !== -1) {
-    const nomePessoaRemovida = pessoas[indiceParaRemover].nome;
-    pessoas.splice(indiceParaRemover, 1);
+    const nomePessoaRemovida = listaPessoas[indiceParaRemover].nome;
+    listaPessoas.splice(indiceParaRemover, 1);
+    exibirRegistros(listaPessoas); // Atualiza a lista no HTML
     return `${nomePessoaRemovida} foi removido(a).`;
   } else {
     return "Pessoa não encontrada.";
   }
 }
 
-// Função para alterar o nome ou a biografia de uma pessoa pelo ID
-function alterarRegistroPorID(id: number, campo: string, novoTexto: string) {
+// Função para alterar o nome ou a biografia de uma pessoa pelo ID no paradigma funcional
+const alterarRegistroPorIDFuncional = (id: number, campo: string, novoTexto: string, listaPessoas: Pessoa[]): string => {
+  if (novoTexto === null) {
+    return "Texto inválido.";
+  }
+
+  const pessoaEncontrada = listaPessoas.find(pessoa => pessoa.id === id);
+
+  if (pessoaEncontrada) {
+    const pessoaModificada = { ...pessoaEncontrada };
+
+    if (campo === "nome") {
+      pessoaModificada.nome = novoTexto;
+    } else if (campo === "biografia") {
+      pessoaModificada.biografia = novoTexto;
+    }
+
+    const indicePessoa = listaPessoas.findIndex(pessoa => pessoa.id === id);
+    listaPessoas[indicePessoa] = pessoaModificada;
+
+    exibirRegistros(listaPessoas); // Atualiza a lista no HTML
+    return "Alteração feita com sucesso.";
+  } else {
+    return "Pessoa não encontrada.";
+  }
+};
+
+// Função para alterar o nome ou a biografia de uma pessoa pelo ID no paradigma imperativo
+function alterarRegistroPorIDImperativo(id: number, campo: string, novoTexto: string, listaPessoas: Pessoa[]): string {
   let indiceParaAlterar = -1;
 
-  // Encontra o índice do objeto especificado pelo ID
-  for (let i = 0; i < pessoas.length; i++) {
-    if (pessoas[i].id === id) {
+  for (let i = 0; i < listaPessoas.length; i++) {
+    if (listaPessoas[i].id === id) {
       indiceParaAlterar = i;
       break;
     }
   }
 
-  if (novoTexto === null)
+  if (novoTexto === null) {
     return "Texto inválido.";
+  }
 
-  // Altera o objeto no índice encontrado
   if (indiceParaAlterar !== -1) {
     if (campo === "nome") {
-      pessoas[indiceParaAlterar].nome = novoTexto;
+      listaPessoas[indiceParaAlterar].nome = novoTexto;
     } else if (campo === "biografia") {
-      pessoas[indiceParaAlterar].biografia = novoTexto;
+      listaPessoas[indiceParaAlterar].biografia = novoTexto;
     }
+
+    exibirRegistros(listaPessoas); // Atualiza a lista no HTML
+    return "Alteração feita com sucesso.";
   } else {
     return "Pessoa não encontrada.";
   }
-
-  return "Alteração feita com sucesso.";
 }
 
 // Função para exibir registros na página HTML
@@ -126,13 +188,13 @@ document.addEventListener("DOMContentLoaded", function () {
   const botaoRemoverRegistro = document.getElementById("removerRegistro") as HTMLButtonElement;
 
   // Obtém elementos de resposta
-  const respDivForm1 =  document.getElementById("respForm1") as HTMLDivElement;
-  const respDivForm2 =  document.getElementById("respForm2") as HTMLDivElement;
+  const respDivForm1 = document.getElementById("respForm1") as HTMLDivElement;
+  const respDivForm2 = document.getElementById("respForm2") as HTMLDivElement;
 
   let resposta: string;
 
   // Formulário para consulta de nome, biografia ou remoção
-  formConsulta.addEventListener('submit', function(event) {
+  formConsulta.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const formData = new FormData(formConsulta);
@@ -140,15 +202,15 @@ document.addEventListener("DOMContentLoaded", function () {
     const id: number = parseInt(inputID);
 
     if (event.submitter == botaoMostrarNome) {
-      resposta = obterNomePorID(id);
+      resposta = obterNomePorIDFuncional(id, pessoas);
       console.log(resposta);
       respDivForm1.textContent = resposta;
     } else if (event.submitter == botaoMostrarBiografia) {
-      resposta = obterBiografiaPorID(id, pessoas); // Adicione 'pessoas' como segundo argumento
+      resposta = obterBiografiaPorIDFuncional(id, pessoas);
       console.log(resposta);
       respDivForm1.textContent = resposta;
     } else if (event.submitter == botaoRemoverRegistro) {
-      resposta = removerPessoaPorID(id);
+      resposta = removerPessoaPorIDFuncional(id, pessoas);
       console.log(resposta);
       respDivForm1.textContent = resposta;
     }
@@ -158,7 +220,7 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   // Formulário para alteração em um item na lista
-  formAlteracao.addEventListener('submit', function(event) {
+  formAlteracao.addEventListener('submit', function (event) {
     event.preventDefault();
 
     const formData = new FormData(formAlteracao);
@@ -168,7 +230,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const campo: string = formData.get("campo") as string;
     const novoConteudo: string = formData.get("novoconteudo") as string;
 
-    resposta = alterarRegistroPorID(id, campo, novoConteudo);
+    resposta = alterarRegistroPorIDFuncional(id, campo, novoConteudo, pessoas);
     console.log(resposta);
     respDivForm2.textContent = resposta;
 
